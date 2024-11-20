5b79d38c35f973cd8e0025fa294f20826c8d8134
yousee360 <info@yousee360.com>
2024-11-20 14:24:55 -0500

    4642 tour

---
+++ app-files/data.js
@@ -0,0 +1,455 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom-4642-saint-laurent-1",
+      "name": "Bedroom 4642 Saint Laurent-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.0237829156584803,
+        "pitch": 0.3185623090954195,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0443489999608335,
+          "pitch": 1.0190722691271148,
+          "rotation": 0,
+          "target": "1-bedroom-4642-saint-laurent-2"
+        },
+        {
+          "yaw": 3.139295967575186,
+          "pitch": 0.19974195403479023,
+          "rotation": 0,
+          "target": "2-living-room-4642-saint-laurent-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.39709285849926,
+          "pitch": -0.32062467145277296,
+          "title": "Balcony",
+          "text": "Sun comes in the morning until early afternoon"
+        },
+        {
+          "yaw": 2.535339029434362,
+          "pitch": 0.16695456013784238,
+          "title": "Closet",
+          "text": "Large closet with plenty of space"
+        }
+      ]
+    },
+    {
+      "id": "1-bedroom-4642-saint-laurent-2",
+      "name": "Bedroom 4642 Saint Laurent-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.3420558598157744,
+        "pitch": 0.5022337296629011,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18654264519374664,
+          "pitch": 0.3426423786359525,
+          "rotation": 0,
+          "target": "2-living-room-4642-saint-laurent-3"
+        },
+        {
+          "yaw": 2.814713716033575,
+          "pitch": 0.935089318098882,
+          "rotation": 0,
+          "target": "0-bedroom-4642-saint-laurent-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0956715915143107,
+          "pitch": 0.17389672968073455,
+          "title": "Closet",
+          "text": "Large closet with plenty of space"
+        },
+        {
+          "yaw": 2.0542042032923344,
+          "pitch": -0.28746179802699245,
+          "title": "Balcony",
+          "text": "Sun comes in the morning until early afternoon"
+        },
+        {
+          "yaw": 1.3609585756394917,
+          "pitch": 0.7505208590742143,
+          "title": "Workspace 2",
+          "text": "TV has amazon firestick for your convenience."
+        }
+      ]
+    },
+    {
+      "id": "2-living-room-4642-saint-laurent-3",
+      "name": "Living Room 4642 Saint Laurent-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.019192979351458916,
+        "pitch": 0.42947687499874654,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9203073396958992,
+          "pitch": 0.2286882127137151,
+          "rotation": 0,
+          "target": "1-bedroom-4642-saint-laurent-2"
+        },
+        {
+          "yaw": 2.152010789003203,
+          "pitch": 0.14634363563595798,
+          "rotation": 0,
+          "target": "3-kitchen---4642-saint-laurent-4"
+        },
+        {
+          "yaw": 3.0433118434255624,
+          "pitch": 0.023292587449812885,
+          "rotation": 0,
+          "target": "4-bathroom---4642-saint-laurent-5"
+        },
+        {
+          "yaw": -2.4135507984203315,
+          "pitch": 0.15413410450800313,
+          "rotation": 0,
+          "target": "5-balcony"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.023925635519003,
+          "pitch": 0.07317992309006449,
+          "title": "Storage space",
+          "text": "Large storage space"
+        },
+        {
+          "yaw": -0.8737522563909952,
+          "pitch": 0.47437715587695095,
+          "title": "Work Station",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4464140736453803,
+          "pitch": 0.5192083421493763,
+          "title": "Sofa-bed",
+          "text": "Can unfold to create extra bed."
+        },
+        {
+          "yaw": 1.9806740295823149,
+          "pitch": 0.1310492822763596,
+          "title": "Main Entrance",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-kitchen---4642-saint-laurent-4",
+      "name": "Kitchen - 4642 Saint Laurent-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3291876458996068,
+        "pitch": 0.10671640052688502,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.022823536372234443,
+          "pitch": 0.027764163656287977,
+          "rotation": 0,
+          "target": "2-living-room-4642-saint-laurent-3"
+        },
+        {
+          "yaw": -1.719897673809374,
+          "pitch": 0.28717554372460086,
+          "rotation": 0,
+          "target": "5-balcony"
+        },
+        {
+          "yaw": -0.8603267910014232,
+          "pitch": 0.16473796771897398,
+          "rotation": 0,
+          "target": "1-bedroom-4642-saint-laurent-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.102107864692119,
+          "pitch": 0.31504552584554624,
+          "title": "Pantry",
+          "text": "6 Large storage spaces available<div>Comes with everything you need, glasses, cups, utensils, bowls.</div>"
+        },
+        {
+          "yaw": 2.56132566144752,
+          "pitch": 0.16825149435770825,
+          "title": "New Fridge",
+          "text": "Two freezer drawers"
+        },
+        {
+          "yaw": -2.7856466164727713,
+          "pitch": 0.3209084642733995,
+          "title": "Spice or Medicine Rack&nbsp;",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.447265265123372,
+          "pitch": 0.6153478659447309,
+          "title": "Fully Functional Oven and Stove",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.1109594618130512,
+          "pitch": 0.4027573243876663,
+          "title": "Water Kettle",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.449353934627335,
+          "pitch": -0.6576758517991337,
+          "title": "5 storage spaces",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.004343117463362134,
+          "pitch": 0.585467493793633,
+          "title": "Double sink and retractable hose",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6947037929148472,
+          "pitch": 0.8788412248295074,
+          "title": "Cookware",
+          "text": "Pots and pans"
+        },
+        {
+          "yaw": -0.4544662801090169,
+          "pitch": 1.0329615049098528,
+          "title": "Garbage and Recycling",
+          "text": "Comes with cleaning supplies<div>Garbage taken out Sunday night</div><div>Recycling and Composte Thursday morning.&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-bathroom---4642-saint-laurent-5",
+      "name": "Bathroom - 4642 Saint Laurent-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.672655807961302,
+        "pitch": 0.2282881100193439,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6131775815649654,
+          "pitch": 0.6911648617253334,
+          "rotation": 0,
+          "target": "2-living-room-4642-saint-laurent-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.095605937060684,
+          "pitch": 0.0013752273534759496,
+          "title": "Pharmacy",
+          "text": "Three storage shelves"
+        },
+        {
+          "yaw": -2.7740170141560654,
+          "pitch": 1.1298385304973007,
+          "title": "Extra Storage",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.660918260524042,
+        "pitch": 0.02739006333204408,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.117285202469544,
+          "pitch": 0.12720929918207702,
+          "rotation": 0,
+          "target": "3-kitchen---4642-saint-laurent-4"
+        },
+        {
+          "yaw": 2.117815574451905,
+          "pitch": 0.038992185758623066,
+          "rotation": 0,
+          "target": "2-living-room-4642-saint-laurent-3"
+        },
+        {
+          "yaw": 0.7283136920893529,
+          "pitch": 0.0864672373612585,
+          "rotation": 0,
+          "target": "0-bedroom-4642-saint-laurent-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2992552563124704,
+          "pitch": 0.4347324687602967,
+          "title": "Balcony Exit",
+          "text": "Leads to courtyard and laundromat. Exit on Villneuve street"
+        },
+        {
+          "yaw": -0.678622049528375,
+          "pitch": -0.10722902276146229,
+          "title": "Mount Royal",
+          "text": "Mountain is always visible right from the balcony.&nbsp;<div>Suprisingly quiet balcony for the location!</div>"
+        }
+      ]
+    }
+  ],
+  "name": "4642 Saint Laurent",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
